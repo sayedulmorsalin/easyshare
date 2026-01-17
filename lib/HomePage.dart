29f08caa3046a82ad2528791600b60cb83d60689
Md. Sayedul Morsalin <164027880+sayedulmorsalin@users.noreply.github.com>
2026-01-17 21:37:57 +0600

    home page done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,8 @@
+
+import 'package:easyshare/views/image_receiver.dart';
+import 'package:easyshare/views/image_sender.dart';
 import 'package:flutter/material.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -11,12 +15,75 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome to easyshare"),
+        title: Text(
+          "Welcome to easyshare",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
       ),
-      body:
-      SafeArea(child: Column(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {
 
-      )),
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ImageSender()));
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.send,
+                      size: 48,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(height: 6),
+                    Text(
+                      'Send',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {
+
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => ImageReceiver()));
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.get_app,
+                      size: 48,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(height: 6),
+                    Text(
+                      'Receive',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/views/image_sender.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageSender extends StatefulWidget {
+  const ImageSender({super.key});
+
+  @override
+  State<ImageSender> createState() => _ImageSenderState();
+}
+
+class _ImageSenderState extends State<ImageSender> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
